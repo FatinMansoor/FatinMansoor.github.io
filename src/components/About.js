52111a2c3423054b8e19c0e5d856eba56bb49206
Fatin Mansoor <aaraf2001@gmail.com>
2025-01-30 21:13:33 -0800

    Added About Me section

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return (
+        <section id = "About">
+            <h2>About Me</h2>
+            <p>
+                I am an undergraduate student at Simon Fraser University
+            </p>
+        </section>
+    );
+};
+
+export default About;
