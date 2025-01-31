b821256104cf621a9d5ddc33e7763edabd1a5e3c
Fatin Mansoor <aaraf2001@gmail.com>
2025-01-30 21:16:26 -0800

    Added Projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+const Projects = () => {
+    return(
+        <section id = "Projects">
+            <h2>Projects</h2>
+            <p>Currently, I'm working on my porfolio website. More projects are under development</p>
+        </section>
+    );
+};
+
+export default Projects;
