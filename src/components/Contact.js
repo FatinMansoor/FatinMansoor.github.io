5b5803477acf0b183de5fa04bfc8146602343912
Fatin Mansoor <aaraf2001@gmail.com>
2025-01-30 21:49:14 -0800

    Added contact section

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+const Contact = () => {
+    return (
+        <section id = "Contact">
+            <h2>Contact Me</h2>
+            <p> Email: aaraf2001@gmail.com / fsm3@sfu.ca</p>
+            <p> LinkedIn: https://www.linkedin.com/in/fatin-mansoor-09001b161/</p>
+            <p> GitHub: https://github.com/FatinMansoor</p>
+        </section>
+    );
+};
+
+export default Contact;
