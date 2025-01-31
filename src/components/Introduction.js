370e3e2b7ce3833879f13175ef1361774249f6d8
Fatin Mansoor <aaraf2001@gmail.com>
2025-01-30 21:08:17 -0800

    Added Introduction

---
+++ src/components/Introduction.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Introduction.css";
+
+const Introduction = () => {
+    return(
+        <section className="Introduction">
+            <h1>Hi, I am Fatin Sadab Mansoor 👋</h1>
+            <p>Second Year Computer Science student at Simon Fraser University || Mathematics Enthusiast</p>
+            <a href = "#projects" className="button">View Projects</a>s
+        </section>
+    );
+};
+
+export default Introduction;
