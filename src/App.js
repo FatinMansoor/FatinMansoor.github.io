9323458848a63d070a5b8418d2386d2a8b9cc391
Fatin Mansoor <aaraf2001@gmail.com>
2025-02-02 20:08:53 -0800

    Updated dark mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //import React from "react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/navbar";
 import Introduction from "./components/introduction";
 import About from "./components/about";
@@ -9,12 +9,27 @@ import Contact from "./components/contact";
 import "./styles/global.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("theme")==="dark";
+  });
 
-  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () =>{
+    setDarkMode((prevMode) => {
+      const newMode = !prevMode;
+      localStorage.setItem("theme", newMode ? "dark" : "light");
+      return newMode;
+    });
+  };
 
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  }
+  useEffect (() => {
+    if(darkMode){
+      document.body.classList.add("dark-mode");
+      document.body.classList.remove("light-mode");
+    } else{
+      document.body.classList.add("light-mode");
+      document.body.classList.add("dark-mode");
+    }
+  }, [darkMode]);
 
   return (
     <div className={darkMode ? "dark-mode" : "light-mode"}>
