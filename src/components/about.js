62fdb688a2c289816926915bcdf1582a135d20b1
Fatin Mansoor <aaraf2001@gmail.com>
2025-02-07 15:19:58 -0800

    Added more information under About Me tab

--- src/components/about.js
+++ src/components/about.js
@@ -3,7 +3,10 @@ const About = () => {
         <section id = "About">
             <h2>About Me</h2>
             <p>
-                I am an undergraduate student at Simon Fraser University.
+                A Computer Science student with a passion for Mathematics and numbers.  
+            </p>
+            <p> 
+                During my spare time I love to play video games, go to gym, go out and travel or sleep!
             </p>
         </section>
     );
