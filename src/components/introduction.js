366820ae3d9ea2b2ab34c02b1ab807a6877e197d
Fatin Mansoor <aaraf2001@gmail.com>
2025-02-07 16:28:56 -0800

    Changed the styling of the navbar

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -5,9 +5,11 @@ const Introduction = () => {
     return(
         <section className="introduction">
             <img src = "/profile_pic.jpg" alt = "Fatin Sadab Mansoor" className="profile-pic" />
-            <h1 className="animated-text">Hi, I am Fatin Sadab Mansoor 👋</h1>
-            <p className="animated-text">Second Year Computer Science student at Simon Fraser University || Mathematics Enthusiast</p>
-            <a href = "#projects" className="button animated-text">View Projects</a>
+            <div className="introduction-content">
+                <h1 className="animated-text">Hi, I am Fatin Sadab Mansoor 👋</h1>
+                <p className="animated-text">Second Year Computer Science student at Simon Fraser University || Mathematics Enthusiast</p>
+                <a href = "#projects" className="button animated-text">View Projects</a>
+            </div>
         </section>
     );
 };
