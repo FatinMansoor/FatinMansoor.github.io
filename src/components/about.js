7c4b87d13ce90367a04a276ec9670a5d8aece4a6
Fatin Mansoor <aaraf2001@gmail.com>
2025-02-09 17:43:59 -0800

    Added my hobby in about me

--- src/components/about.js
+++ src/components/about.js
@@ -5,6 +5,9 @@ const About = () => {
             <p>
                 A Computer Science student with a passion for Mathematics and numbers.  
             </p>
+            <p> 
+                During my spare time I love to play video games, go to gym, go out and travel or sleep!
+            </p>
         </section>
     );
 };
