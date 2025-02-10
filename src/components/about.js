9e126ea555f74817dad9e86c3e08b152f5e40c63
Fatin Mansoor <aaraf2001@gmail.com>
2025-02-09 17:41:19 -0800

    Fixed grammatical error on About me Page

--- src/components/about.js
+++ src/components/about.js
@@ -5,9 +5,6 @@ const About = () => {
             <p>
                 A Computer Science student with a passion for Mathematics and numbers.  
             </p>
-            <p> 
-                During my spare time I love to play video games, go to gym, go out and travel or sleep!
-            </p>
         </section>
     );
 };
