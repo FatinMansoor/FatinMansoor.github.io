2e9f18ba59ef94bd291626dc9093748a8af7823e
Fatin Mansoor <aaraf2001@gmail.com>
2025-02-11 21:39:06 -0800

    Added more pictures

--- src/components/about.js
+++ src/components/about.js
@@ -2,12 +2,14 @@ const About = () => {
     return (
         <section id = "About">
             <h2>About Me</h2>
-            <p>
-                A Computer Science student with a passion for mathematics and numbers.  
-            </p>
-            <p> 
+            <p> A Computer Science student with a passion for mathematics and numbers.</p>
+
+            <div className="about-me">
+                    <p> 
                 During my spare time I love to play video games, go to the gym, explore food options in the city, travel or sleep!
-            </p>
+                </p>
+                <img src = "/sunset.png" alt = "Sunset in Forza Horizon 5" className="about-image" />
+            </div>
         </section>
     );
 };
