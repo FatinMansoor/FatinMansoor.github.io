83ed70a1912afc7946c30d1fe14038903b5e8dab
Fatin Mansoor <aaraf2001@gmail.com>
2025-05-11 14:52:53 -0700

    Added link to Harmoniq

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,7 +2,17 @@ const Projects = () => {
     return(
         <section id = "projects" className="projects">
             <h2>Projects</h2>
-            <p>Currently, I'm working on my portfolio website. More projects are under development.</p>
+            <h3>
+                <a 
+                href="https://harmoniqmusic.vercel.app/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style = {{ color: '#f5c542', textDecoration: 'none' }}
+                >
+                    Harmoniq: A Music sharing App
+                </a>
+
+            </h3>
         </section>
     );
 };
