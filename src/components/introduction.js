89d2253f68aeba1670115ffac928b2ceccc5f101
Fatin Mansoor <aaraf2001@gmail.com>
2025-05-12 01:29:19 -0700

    Changed Intro

--- src/components/introduction.js
+++ src/components/introduction.js
@@ -7,7 +7,7 @@ const Introduction = () => {
             <img src = "/profile_pic.jpg" alt = "Fatin Sadab Mansoor" className="profile-pic" />
             <div className="introduction-content">
                 <h1 className="animated-text">Hi, I am Fatin Sadab Mansoor 👋</h1>
-                <p className="animated-text">Second Year Computer Science student at Simon Fraser University || Mathematics Enthusiast</p>
+                <p className="animated-text">Third Year Data Science student at Simon Fraser University || Mathematics Enthusiast</p>
                 <a href = "#projects" className="button animated-text">View Projects</a>
             </div>
         </section>
