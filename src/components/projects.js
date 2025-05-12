77887a207c417893d024c07488bb64c2861dd3fd
Fatin Mansoor <aaraf2001@gmail.com>
2025-05-12 02:13:01 -0700

    Removed

--- src/components/projects.js
+++ src/components/projects.js
@@ -2,15 +2,14 @@ const Projects = () => {
     return(
         <section id = "projects" className="projects">
             <h2>Projects</h2>
-        <h3 style={{ textAlign: "center" }}>
-            🎵 Check out my latest project:{" "}
+        <h3 style={{ textAlign: "center", fontSize: "1.1rem" }}>
+            🎵{" "}
              <a
                 href="https://harmoniqmusic.vercel.app/"
                 target="_blank"
                 rel="noopener noreferrer"
                 style={{ color: "#FADA5E", textDecoration: "underline" }}
             >
-    
                 Harmoniq – A Music Sharing App https://harmoniqmusic.vercel.app/
             </a>
         </h3>
