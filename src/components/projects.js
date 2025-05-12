7ab66f1ebd945a347563a0f29e2aa21c098afa3f
Fatin Mansoor <aaraf2001@gmail.com>
2025-05-12 02:18:24 -0700

    Changed project.js

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,18 +1,17 @@
 const Projects = () => {
-    return(
-        <section id = "projects" className="projects">
+    return (
+        <section id="projects" className="projects">
             <h2>Projects</h2>
-        <h3 style={{ textAlign: "center", fontSize: "1.1rem" }}>
-             <a
-                href="https://harmoniqmusic.vercel.app/"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#FADA5E", textDecoration: "none" }}
-            >
-                Harmoniq – A Music Sharing App 🎵
-            </a>
-        </h3>
-
+            <h3 style={{ textAlign: "center", fontSize: "1.1rem" }}>
+                <a
+                    href="https://harmoniqmusic.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ color: "#FADA5E", textDecoration: "none" }}
+                >
+                    Harmoniq – A Music Sharing App 🎵
+                </a>
+            </h3>
         </section>
     );
 };
